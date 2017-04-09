c4758b36ba7d1e2b3d86233a4e3b175453a0525c
Rod Melo <rodmelo@fromflux.com>
2017-04-09 01:06:04 +0100

    Add favicon

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 
-const port = (process.env.PORT || 3000)
-const indexPath = path.join(__dirname, '/public/index.html')
-const publicPath = express.static(path.join(__dirname, '/public'))
+const port = (process.env.PORT || 3000);
+const indexPath = path.join(__dirname, '/public/index.html');
+const publicPath = express.static(path.join(__dirname, '/public'));
 
-const app = express()
+const app = express();
 
-app.use(publicPath)
+app.use(publicPath);
 
-app.get('*', function (req, res) { res.sendFile(indexPath) })
+app.get('*', function (req, res) { res.sendFile(indexPath); });
 
-app.listen(port)
+app.listen(port);
 
-console.log(`Listening at http://localhost:${port}`)
+console.log(`Listening at http://localhost:${port}`);
